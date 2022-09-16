26f32610acee215432ca191a17f8e1193724ea05
unknown <unsungpoetry1993@gmail.com' git config --global user.name 00julianthompsongit config user.name  git commit -m Added>
2022-09-16 18:08:15 -0400

    starting lightswitch page

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+
+
+function Navbar() {
+    return (
+<div className="navBar">
+    <div className="navLinks">
+        
+        <a href="/">Home</a>
+        <a href="/About">About</a>
+    </div>
+          
+</div>    
+)
+
+}
+
+      export default Navbar
